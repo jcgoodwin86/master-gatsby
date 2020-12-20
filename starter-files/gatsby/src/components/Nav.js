f17254a45e61d4ef9eaebf239afc4af0382f4588
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2020-12-20 15:21:44 -0500

    set global styles

--- starter-files/gatsby/src/components/Nav.js
+++ starter-files/gatsby/src/components/Nav.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+  background: green;
+`;
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Hot Now</Link>
-        </li>
-        <li>
-          <Link to="/pizzas">Pizzas</Link>
-        </li>
-        <li>
-          <Link to="/">LOGO</Link>
-        </li>
-        <li>
-          <Link to="/slicemasters">Slicemasters</Link>
-        </li>
-        <li>
-          <Link to="/order">Order Ahead!</Link>
-        </li>
-      </ul>
-    </nav>
+    <NavStyles>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Hot Now</Link>
+          </li>
+          <li>
+            <Link to="/pizzas">Pizzas</Link>
+          </li>
+          <li>
+            <Link to="/">LOGO</Link>
+          </li>
+          <li>
+            <Link to="/slicemasters">Slicemasters</Link>
+          </li>
+          <li>
+            <Link to="/order">Order Ahead!</Link>
+          </li>
+        </ul>
+      </nav>
+    </NavStyles>
   );
 }
