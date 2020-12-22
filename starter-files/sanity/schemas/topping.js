0ca7ca82e66138c37377e97ff1f6f7cf11e6b11d
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2020-12-22 14:29:21 -0500

    added schemas

---
+++ starter-files/sanity/schemas/topping.js
@@ -0,0 +1,35 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+  // visible name
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
